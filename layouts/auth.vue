e3acfced765a8edea7b040cc412bd4c533a9c96a
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-06 15:12:37 +0800

    feat: pre initial setup
    - add routes and layouts and pages (sigin in, sign up, forgot password, otp, reset, home)
    - adding colors and typography

---
+++ layouts/auth.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-neutral-100 min-h-screen">
+    <span>LOGIN LAYOUT</span>
+    <slot />
+  </div>
+</template>
---
+++ pages/forgot-password/otp.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center">
+        <h2 class="text-2xl font-bold mb-4">Wellcome to Forgot Password - OTP</h2>
+    </div>
+</template>
