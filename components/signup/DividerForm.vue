231f280f00e60871242fcff33c486e947d0a29aa
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-07 09:02:05 +0800

    feat: adding a separator border in divider forms

--- components/signup/DividerForm.vue
+++ components/signup/DividerForm.vue
@@ -1,7 +1,9 @@
 <template>
-    <div className="flex mt-2 gap-4 items-center justify-center" data-testid="divider-form">
-        <span className="text-body-normal text-neutral-400" data-testid="divider-text">
+    <div class="flex items-center w-lg gap-4 justify-center" data-testid="divider-form">
+        <hr class="flex-1 border-t border-neutral-200" />
+        <span class="text-body-normal text-neutral-400" data-testid="divider-text">
             {{ $t("login.form.signIn.or") }}
         </span>
+        <hr class="flex-1 border-t border-neutral-200" />
     </div>
 </template>
