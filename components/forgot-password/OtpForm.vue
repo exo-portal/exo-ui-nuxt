c905784e2f30748c3cbe1e4e9164262712a4e5b7
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-07 21:05:33 +0800

    feat: add form and header otp

--- components/forgot-password/OtpForm.vue
+++ components/forgot-password/OtpForm.vue
@@ -1,10 +1,11 @@
 <template>
-    <div className="flex flex-col gap-2 text-center min-w-lg mt-32 mb-10">
+    <div class="">Otp Form</div>
+    <!-- <div className="flex flex-col gap-2 text-center min-w-lg mt-32 mb-10">
         <h1 className="text-sub-heading-4 font-bold text-neutral-950">
             {{ $t("forgotPassword.form.otp.header.title") }}
         </h1>
         <p className="text-neutral-500 text-body-normal">
             {{ $t("forgotPassword.form.otp.header.subtitle") }}
         </p>
-    </div>
+    </div> -->
 </template>
