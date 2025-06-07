bb591eef62940b7a834ef04dd764388794554a59
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-07 08:24:04 +0800

    feat: adding routings for each roles home and profile page

---
+++ pages/internal/hr/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { metaTItleBuilder } from '~/lib/utils';
+
+useHead({
+    title: metaTItleBuilder('HR'),
+})
+</script>
+<template>
+    <div>
+        <h1>HR</h1>
+        <p>This page will display the HR home/dashboard.</p>
+    </div>
+</template>
---
+++ pages/internal/project-team/profile/personal-details.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { metaTItleBuilder } from '~/lib/utils';
+
+useHead({
+    title: metaTItleBuilder('Project Team | Profile | Personal Details'),
+})
+</script>
+
+<template>
+    <div>
+        <h1>Project Team</h1>
+        <p>This page will display the project team details. | Personal details</p>
+    </div>
+</template>
