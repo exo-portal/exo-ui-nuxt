02d5b5769c445f7e02fce036fc3e618479d97678
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-07 23:47:12 +0800

    feat: adding icons (github/calendar, chevron down, google,user)

--- assets/svg/index.ts
+++ assets/svg/index.ts
@@ -1,5 +1,17 @@
 import BackIcon from "./BackIcon.svg";
 import PasswordEyeIcon from "./PasswordEyeIcon.svg";
 import PasswordEyeSlashIcon from "./PasswordEyeSlashIcon.svg";
+import GoogleIcon from "./GoogleIcon.svg";
+import GithubIcon from "./GithubIcon.svg";
+import CalendarIcon from "./CalendarIcon.svg";
+import ChevronDownIcon from "./ChevronDownIcon.svg";
 
-export { BackIcon, PasswordEyeIcon, PasswordEyeSlashIcon };
+export {
+  BackIcon,
+  PasswordEyeIcon,
+  PasswordEyeSlashIcon,
+  GoogleIcon,
+  GithubIcon,
+  CalendarIcon,
+  ChevronDownIcon,
+};
