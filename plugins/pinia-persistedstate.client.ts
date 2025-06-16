0fc28e492d7125378e065531152a5d9695594791
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-16 22:02:32 +0800

    feat(auth): use pinia persistedstate afterRestore hook for hydration detection

--- plugins/pinia-persistedstate.client.ts
+++ plugins/pinia-persistedstate.client.ts
@@ -5,10 +5,4 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 export default defineNuxtPlugin((nuxtApp) => {
   const pinia = nuxtApp.$pinia as Pinia;
   pinia.use(piniaPluginPersistedstate);
-
-  // Set hydrated flag after hydration
-  setTimeout(() => {
-    const auth = useAuthStore();
-    auth.hydrated = true;
-  }, 0);
 });
