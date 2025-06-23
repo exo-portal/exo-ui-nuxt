8bbf2070db58f85888cd280c81e4e16821ec6af0
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-23 09:30:54 +0800

    feat: adding dropdown on click profile for top nav

--- components/common/TopNav.vue
+++ components/common/TopNav.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import { ChevronRight, LogOutIcon } from 'lucide-vue-next';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import { Separator } from '../ui/separator';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu';
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
+
+const router = useRouter();
+
+async function handleLogout() {
+    logoutUser().then(() => {
+        router.push('/signin');
+    }).catch((error) => {
+    });
+}
 </script>
 
 <template>
@@ -22,12 +34,38 @@ import { Separator } from '../ui/separator';
             <div class="flex items-center space-x-4 p-4  bg-white rounded-4xl shadow-[0_0_24px_0_rgba(0,0,0,0.10)]">
                 <div class="">Notification</div>
                 <div class="h-[40px] border-l-2"></div>
-                <button id="profile-menu" aria-haspopup="true" aria-expanded="false" aria-controls="profile-dropdown">
-                    <Avatar>
-                        <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                </button>
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel as="div" class="p-2">
+                            <NuxtLink :to="`${route.path}/profile`"
+                                class="flex items-center space-x-4 cursor-pointer no-underline focus:outline-none"
+                                aria-label="Go to profile">
+                                <div class="flex flex-col">
+                                    <h3 class="text-body-normal font-bold text-neutral-800">Rochenette Legaspina</h3>
+                                    <p class="text-body-small text-neutral-400">rochenette@gmail.com</p>
+                                </div>
+                                <ChevronRight class="text-neutral-400" />
+                            </NuxtLink>
+                        </DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Profile</DropdownMenuItem>
+                        <DropdownMenuItem>Billing</DropdownMenuItem>
+                        <DropdownMenuItem>Team</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem class="flex gap-2 items-center" @click="handleLogout">
+                            <LogOutIcon class="text-neutral-400" />
+                            <span class="text-neutral-400 text-body-normal">
+                                Logout
+                            </span>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
     </nav>
